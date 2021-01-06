ce8677e6b87b776567e9af41003f9241fa77260c
JoHoop <jolienhoop@gmail.com>
2021-01-06 13:39:02 +0100

    Add toggle for dark mode

---
+++ src/Theme/ThemeToggle.js
@@ -0,0 +1,42 @@
+import React, { useContext } from "react";
+import { ThemeContext } from "./ThemeProvider";
+import { Tooltip, IconButton, Zoom } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Brightness4Icon from "@material-ui/icons/Brightness4";
+import Brightness7Icon from "@material-ui/icons/Brightness7";
+
+const useStyles = makeStyles((theme) => ({
+  iconButton: {
+    height: "2.5rem",
+    width: "2.5rem",
+  },
+  icon: {
+    fontSize: "1.25rem",
+  },
+}));
+
+export const ThemeToggle = () => {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+  const classes = useStyles();
+
+  return (
+    <Tooltip
+      title={"Toggle light/dark theme"}
+      placement="right"
+      TransitionComponent={Zoom}
+    >
+      <IconButton
+        color="inherit"
+        onClick={toggleTheme}
+        aria-label={"Toggle light/dark theme"}
+        className={classes.iconButton}
+      >
+        {theme === "light" ? (
+          <Brightness4Icon className={classes.icon} />
+        ) : (
+          <Brightness7Icon className={classes.icon} />
+        )}
+      </IconButton>
+    </Tooltip>
+  );
+};
