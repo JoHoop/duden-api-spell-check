09d7a807af111543605adfe9a5ff2ff0aad5f007
JoHoop <jolienhoop@gmail.com>
2021-01-06 15:34:37 +0100

    Add getters for editor state

--- src/Components/EditorWrapper.js
+++ src/Components/EditorWrapper.js
@@ -2,10 +2,31 @@ import React from "react";
 import { Editor, EditorState } from "draft-js";
 import "draft-js/dist/Draft.css";
 
+const getCurrentBlock = (editorState) => {
+  const currentSelection = editorState.getSelection();
+  const blockKey = currentSelection.getStartKey();
+  return editorState.getCurrentContent().getBlockForKey(blockKey);
+};
+
+const getCurrentLetter = (editorState) => {
+  const currentBlock = getCurrentBlock(editorState);
+  const blockText = currentBlock.getText();
+  return blockText[editorState.getSelection().getStartOffset() - 1];
+};
+
 export const EditorWrapper = () => {
-  const [editorState, setEditorState] = React.useState(() =>
+  const [editorState, setEditorState] = React.useState(
     EditorState.createEmpty()
   );
 
-  return <Editor editorState={editorState} onChange={setEditorState} />;
+  const onChange = (newEditorState) => {
+    const letter = getCurrentLetter(newEditorState);
+    if (getCurrentBlock(newEditorState).getText().length > 9) {
+      setEditorState(editorState);
+      return;
+    }
+    setEditorState(newEditorState);
+  };
+
+  return <Editor editorState={editorState} onChange={onChange} />;
 };
