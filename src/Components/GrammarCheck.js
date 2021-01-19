e313b72f682d29ad79fa9929e422d137c2ef630f
JoHoop <jolienhoop@gmail.com>
2021-01-19 18:25:34 +0100

    Fix cannot find GrammarCheck

--- src/Components/GrammarCheck.js
+++ src/Components/GrammarCheck.js
@@ -3,7 +3,7 @@ import { TextareaAutosize } from "@material-ui/core";
 import { Button } from "@material-ui/core";
 import { EditorWrapper } from "./EditorWrapper";
 
-export const Grammarcheck = () => {
+export const GrammarCheck = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   const [inputValue, setInputValue] = useState(
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -3,7 +3,7 @@ import { Header } from "../Components/Header";
 import { Footer } from "../Components/Footer";
 import { makeStyles } from "@material-ui/core/styles";
 import { Container } from "@material-ui/core";
-import { Grammarcheck } from "../Components/Grammarcheck";
+import { GrammarCheck } from "../Components/GrammarCheck";
 
 const useStyles = makeStyles((theme) => ({
   homePage: {
@@ -20,7 +20,7 @@ export const HomePage = () => {
     <div className={classes.homePage}>
       <Header />
       <Container maxWidth="md">
-        <Grammarcheck />
+        <GrammarCheck />
       </Container>
       <Footer />
     </div>
