c27b323ad50edb2facecfe7b2a4a1fa383753794
JoHoop <jolienhoop@gmail.com>
2021-01-19 18:56:40 +0100

    Fix styling of textarea

--- src/Components/GrammarCheck.js
+++ src/Components/GrammarCheck.js
@@ -1,6 +1,24 @@
 import React, { useState } from "react";
 import { TextareaAutosize } from "@material-ui/core";
 import { Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  textField: {
+    background: "rgba(0, 0, 0, 0)",
+    color: "rgb(250, 250, 250)",
+    padding: "15px",
+    border: "1px solid rgba(0, 191, 191, 0.5)",
+    borderRadius: "4px",
+    overflowY: "hidden",
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    fontSize: "14px",
+    display: "block",
+    fontWeight: "400",
+    textAlign: "start",
+    lineHeight: "20px",
+  },
+});
 
 export const GrammarCheck = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -37,13 +55,16 @@ export const GrammarCheck = () => {
       .catch((error) => console.log(error));
   };
 
+  const classes = useStyles();
+
   return (
     <>
       <TextareaAutosize
-        rowsMax={10}
+        rowsMin={10}
         placeholder="Enter text"
         value={inputText}
         onChange={onChange}
+        className={classes.textField}
       />
 
       <Button variant="outlined" color="primary" onClick={checkSpelling}>
